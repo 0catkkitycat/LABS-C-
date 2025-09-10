4842cfcc4b426cdb0789478e642db49a3a0f7cfe
Gina Ruiz <ginarruizcampos@gmail.com>
2025-09-10 14:24:57 -0600

    lab 1.0.9 terminado

--- 1.0.9_LAB/1.0.9_LAB.cpp
+++ 1.0.9_LAB/1.0.9_LAB.cpp
@@ -1,1 +1,62 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class AdHocSquare
+{
+public:
+	AdHocSquare(double side);
+	void set_side(double side);
+	double get_area()
+	{
+		return side * side; 
+	}
+
+private:
+	double    side;
+};
+
+AdHocSquare::AdHocSquare(double side)
+{
+	this->side = side;
+}
+
+void AdHocSquare::set_side(double new_side) {
+		if (new_side >= 0) {
+		this->side = new_side;
+		}
+}
+
+
+class LazySquare
+{
+public:
+	LazySquare(double side);
+	void set_side(double side);
+	double get_area();
+private:
+	double    side;
+	double    area;
+	bool      side_changed;
+};
+
+LazySquare::LazySquare(double side) {
+	this->side = side;
+	this->area = side * side;
+	this->side_changed = false;
+}
+
+void LazySquare::set_side(double side) {
+	this->side = side;
+	this->side_changed = true;
+}
+
+double LazySquare::get_area() {
+	if (side_changed) {
+		area = side * side;
+	}
+
+	return area;
+}
 
